14f4e98f688dedb6d3ad006e8877bc4f17961195
Nikita Endea <90311455+Nikita-Endeka2004@users.noreply.github.com>
2022-08-03 17:04:18 +0300

    made

---
+++ assets/js/app.js
@@ -0,0 +1,112 @@
+
+
+
+let a = +prompt("Введите а",0); //ФОРМУЛА
+
+let y;
+
+y = ((a**2)+10)/(Math.sqrt((a**2)+1));
+
+y = y * 1000;
+y = Math.round(y);
+y = y/1000;
+
+alert(`y = ${y}`);
+
+
+
+
+
+
+// let cat_1 = +prompt("Ввведите первый катет", 0); //ГИПОТЕНУЗА
+
+// let cat_2 = +prompt("Введите второй катет", 0);
+
+// let sum;
+
+// sum = Math.sqrt(cat_1*cat_1+cat_2*cat_2);
+
+// sum = sum * 10;
+// sum = Math.round(sum);
+// sum = sum/10;
+
+// alert(`Длина гипотенузы ${sum}`);
+
+
+
+
+
+// let dweller = +prompt("Введите количество жителей государства(в чел)", 0); //ГЕОГРАФИЯ
+
+// let square = +prompt("Введите площадь государства(в км^2)",0);
+
+// let sum;
+
+// sum = dweller/square;
+
+// alert(`Плотность государства равняется ${sum} чел/км^2`);
+
+
+
+
+
+
+
+
+
+// let my_volume = +prompt("Введите объем вещества",0); //ПОТНОСТЬ ВЕЩЕСТВА
+
+// let mass = +prompt("Введите массу вещетва", 0);
+
+// let po;  //Плотность
+
+// po = mass * my_volume;
+
+// alert(`Плотность вещества равна ${po}`);
+
+
+
+
+
+
+// let circle_radius = +prompt("Введите радиус окружноти", 0); //КРУГ
+
+// const pi = 3.14;
+
+// let l, S;
+
+// l= 2*pi*circle_radius;
+
+// S = pi*circle_radius*circle_radius;
+
+// S = S*10;
+// S = Math.round(S);
+// S = S/10;
+
+// alert(`Длина окуржности равна ${l}`);
+// alert(`Площадь окружности равна ${S}`);
+
+
+
+
+
+
+// let cude = +prompt("Введите длину ребра куба", 0);  //КУБ
+
+// let S, V;
+
+// S = 6*cude*cude; 
+
+// V = cude*cude*cude;
+
+// alert(`Объем равен ${V}`);
+// alert(`Площадь равна ${S}`);
+
+
+
+
+// let side_square = +prompt("Введите сторону квадрата", 0); // КВАДРАТ
+
+// side_square = side_square * 4;
+
+// alert(`Периметр квадрата равен: ${side_square}`);
